d2de42009c98cda2bab4cc65bd283baf7a7985cf
Ben Fitzhardinge <ben@fitzhardinge.net>
2023-12-07 13:27:07 +0800

    day 06 part 1

---
+++ day_06/day6.py
@@ -0,0 +1,46 @@
+from collections import namedtuple
+from functools import reduce
+from operator import mul
+
+
+def get_distance(hold_time: int, race_time: int) -> float:
+    return hold_time * (race_time - hold_time)
+
+
+def filter_distances(race_time: int, check_distance: int) -> list[float]:
+    return [
+        get_distance(x, race_time)
+        for x in range(1, race_time)
+        if get_distance(x, race_time) > check_distance
+    ]
+
+
+Race = namedtuple("Race", "time distance")
+
+
+def get_races(raw_data: list[str]) -> list[Race]:
+    time, distance = raw_data[0:2]
+    times = [int(val.strip()) for val in time.split(":")[1].split() if val]
+    distances = [int(val.strip()) for val in distance.split(":")[1].split() if val]
+    return [Race(t, d) for t, d in zip(times, distances)]
+
+
+def part_1(raw_data: list[str]) -> int:
+    races = get_races(raw_data)
+    valid_races = [filter_distances(*x) for x in races]
+    return reduce(mul, (len(x) for x in valid_races))
+
+
+def part_2(raw_data: list[str]) -> int:
+    pass
+
+
+def get_raw_data(path: str) -> list[str]:
+    """fetch raw data from file"""
+    with open(path) as f:
+        return [x.strip() for x in f.readlines()]
+
+
+if __name__ == "__main__":
+    print(part_1(get_raw_data("./input.txt")))
+    print(part_2(get_raw_data("./input.txt")))
---
+++ day_06/tests/test_day6.py
@@ -0,0 +1,25 @@
+from .. import day6
+
+TEST_DATA = [
+    "Time:      7  15   30",
+    "Distance:  9  40  200",
+]
+
+
+def test_day6_part1():
+    assert day6.part_1(TEST_DATA) == 288
+
+
+def test_get_distance():
+    assert day6.get_distance(0, 7) == 0
+    assert day6.get_distance(1, 7) == 6
+    assert day6.get_distance(2, 7) == 10
+    assert day6.get_distance(3, 7) == 12
+    assert day6.get_distance(4, 7) == 12
+    assert day6.get_distance(5, 7) == 10
+    assert day6.get_distance(6, 7) == 6
+    assert day6.get_distance(7, 7) == 0
+
+
+def test_filter_distances():
+    assert day6.filter_distances(7, 9) == [10, 12, 12, 10]
